821f9cc0d3926da9580625fa3c474865b025700f
Omkar <148841079+omkar3364@users.noreply.github.com>
2024-08-13 21:49:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+    // You can also add code here to send the form data to a server or an email service.
+});
